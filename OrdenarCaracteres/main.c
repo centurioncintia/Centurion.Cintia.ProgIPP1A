ccecf847fd3bc22004b9f621bd0aa4b56a723016
centurioncintia <cncmultimedios@gmail.com>
2020-11-16 10:48:29 -0300

    parcial

---
+++ OrdenarCaracteres/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//3. Crear una función que se llame ordenarCaracteres que reciba una cadena de caracteres como parámetro y su responsabilidad es ordenarlos caracteres de manera ascendente dentro de la cadena.
+// Ejemplo si le pasamos "algoritmo" la deja como "agilmoort" *
+
+
+ordenarCaracteres(char cadena[]);
+int main()
+{
+
+
+
+    return 0;
+}
+ordenarCaracteres(char cadena[]){
+
+
+
+
+
+
+}
